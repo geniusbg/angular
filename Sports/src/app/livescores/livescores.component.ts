a28a0d8d57e2557d548f18860d548e3b1e645c3f
Nickolay Petroff <geniuss0ft@yahoo.com>
2019-01-25 21:19:36 +0200

    n

--- Sports/src/app/livescores/livescores.component.ts
+++ Sports/src/app/livescores/livescores.component.ts
@@ -1,24 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { LivescoresService } from '../services/livescores.service';
 
+
 @Component({
   selector: 'app-livescores',
   templateUrl: './livescores.component.html',
   styleUrls: ['./livescores.component.css']
 })
 export class LivescoresComponent implements OnInit {
-  livescores:any;
+  livescores: any;
 
-  constructor(private getLivescores : LivescoresService) { }
+  constructor(private getLivescores: LivescoresService) { }
 
   ngOnInit() {
     this.getLivescores.getLivescores().subscribe(data => {
       this.livescores = data
-      console.log(data)
       this.livescores = this.livescores.teams.Match
       console.log(this.livescores)
     })
-  
+
   }
 
 }
