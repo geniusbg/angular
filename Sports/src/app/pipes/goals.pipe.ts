12f679b0be4aa8f3f5874999d801bdb41c4e7e82
Nickolay Petroff <geniuss0ft@yahoo.com>
2019-01-27 14:26:20 +0200

    ref

--- Sports/src/app/pipes/goals.pipe.ts
+++ Sports/src/app/pipes/goals.pipe.ts
@@ -6,8 +6,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class GoalsPipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
-    
-   console.log(value)
+    value = value.split(';')
+    value.forEach(element => {
+      element = element + '\n'
+      console.log(element)
+    });
     return value;
   }
 
