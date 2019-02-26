f280faea942103dd8ed019bdd0c47cecda35ce37
Nickolay Petroff <geniuss0ft@yahoo.com>
2019-02-26 02:05:26 +0200

    refact

--- InterceptHP/src/app/app.module.ts
+++ InterceptHP/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppBootstrapModule } from './bootstap.module';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +11,9 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
+
+import { AuthService } from './authentication/auth.service';
+import { TokenInterceptor } from './interceptors/token.interceptor';
 
 @NgModule({
   declarations: [
@@ -22,9 +27,16 @@ import { FooterComponent } from './shared/footer/footer.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AppBootstrapModule
+    AppBootstrapModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [AuthService,
+  {
+    provide: HTTP_INTERCEPTORS,
+    useClass: TokenInterceptor,
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- InterceptHP/src/app/components/login/login.component.ts
+++ InterceptHP/src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/authentication/auth.service';
+import { Router } from '@angular/router';
+import { LoginModel } from 'src/app/authentication/models/login.model';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  model: LoginModel
+  loginFailed: Boolean
+  errMsg: string
+
+  constructor(private authService: AuthService, private router: Router) {
+    this.model = new LoginModel("", "")
+  }
 
   ngOnInit() {
+  }
+
+  login() {
+    this.authService.login(this.model).subscribe(data => {
+      this.router.navigate([''])
+    },
+      err => {
+        this.loginFailed = true
+        this.errMsg = err.error.description
+      }
+    )
   }
 
 }
